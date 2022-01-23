72cff89c137bb86505e424b6cfccc7a4620a1d3b
JaxxonJargonGames <jaxxonjargon@gmail.com>
2022-01-22 20:34:57 -0800

    Remove some status text.

--- src/StarterGui/ScreenGui/StatusTextLabel/StatusValue.client.lua
+++ src/StarterGui/ScreenGui/StatusTextLabel/StatusValue.client.lua
@@ -2,7 +2,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StarterGui = game:GetService("StarterGui")
 
 local ChooseTeamRemoteEvent = ReplicatedStorage:WaitForChild("ChooseTeamRemoteEvent")
-local FeatureFoundRemoteEvent = ReplicatedStorage:WaitForChild("FeatureFoundRemoteEvent")
 local StatusRemoteEvent = ReplicatedStorage:WaitForChild("StatusRemoteEvent")
 
 local Typewriter = require(ReplicatedStorage:WaitForChild("Typewriter"))
@@ -10,7 +9,6 @@ local Typewriter = require(ReplicatedStorage:WaitForChild("Typewriter"))
 local DELAY_BETWEEN_CHARS = 0.025
 local MESSAGE_WAIT_TIME = 0.2
 
-local featureCount = 0
 local label = script.Parent
 
 -- Load translator if game is localized
@@ -33,18 +31,6 @@ local intro = {
 	[[Welcome to Jaxxon Jargon's<br /><font size="46" color="rgb(255,50,25)">Treasure Island</font> <font size="40">🗡</font>]],
 }
 
-local messages = {
-	
-	[[Your first objective is to find <font color="rgb(255,200,50)">gold!</font><br />Your second objective is to go shopping<br />for weapons and other useful items.]],
-}
-
 ChooseTeamRemoteEvent.OnClientEvent:Connect(function()
 	displayStatus(intro)
-	if featureCount < 5 then
-		displayStatus(messages)
-	end
-end)
-
-FeatureFoundRemoteEvent.OnClientEvent:Connect(function(count, featureName)
-	featureCount = count
 end)
