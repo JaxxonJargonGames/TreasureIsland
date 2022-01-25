dc9242ba6a1044ec9d7e8e999a8c3b180d7cd461
JaxxonJargonGames <jaxxonjargon@gmail.com>
2022-01-24 16:22:05 -0800

    Simplified and fixed a bug.

--- src/ServerScriptService/GoldValueChatCommand.server.lua
+++ src/ServerScriptService/GoldValueChatCommand.server.lua
@@ -1,8 +1,7 @@
 local Players = game:GetService("Players")
 
--- Command to add an amount of gold. (note the trailing space)
-local ADD_COMMAND = "/addgold "
-local SET_COMMAND = "/gold "
+-- Command to set an amount of gold. (note the trailing space)
+local COMMAND = "/gold "
 
 local function hasMatchingCommandName(text, command)
 	-- Note: string.sub(message, ...) is the same as text:sub(...)
@@ -10,18 +9,13 @@ local function hasMatchingCommandName(text, command)
 end
 
 local function getNumberFromMessage(message)
-	local numberString = message:sub(ADD_COMMAND:len() + 1) -- Cut out the "100" from "/addgold 100".
+	local numberString = message:sub(COMMAND:len() + 1) -- Cut out the "100" from "/gold 100".
 	local number = tonumber(numberString)
 	return number
 end
 
 local function onPlayerChatted(player, message, recipient)
-	if hasMatchingCommandName(message, ADD_COMMAND) then
-		local number = getNumberFromMessage(message)
-		if number then
-			player.leaderstats.Gold.Value += number
-		end
-	elseif hasMatchingCommandName(message, SET_COMMAND) then
+	if hasMatchingCommandName(message, COMMAND) then
 		local number = getNumberFromMessage(message)
 		if number then
 			player.leaderstats.Gold.Value = number
