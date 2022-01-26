93594d64d544f9c7495e0f3f91cdc7c60dec37e1
JaxxonJargonGames <jaxxonjargon@gmail.com>
2022-01-26 08:44:25 -0800

    Revert "WIP on top scores."
    This reverts commit 5e9c4943967189a695f5eb4d28c0a1ef50c90e15.

--- src/StarterGui/ScreenGui/TopScoresScrollingFrame/TopScores.client.lua
+++ src/StarterGui/ScreenGui/TopScoresScrollingFrame/TopScores.client.lua
@@ -18,10 +18,7 @@ local function getTextLabel(text)
 end
 
 TopScoresRemoteEvent.OnClientEvent:Connect(function(topScores)
-	frame = script.Parent:WaitForChild("TopScoresScrollingFrame")
-	frame:ClearAllChildren()
-	layout = Instance.new("VerticalListLayout")
-	layout.Parent = frame
+	-- script.Parent:ClearAllChildren()
 	local textLabel = getTextLabel("Top 20 Scores of All Time")
 	textLabel.Font = Enum.Font.GothamBold
 	textLabel.Size = UDim2.new(0, 500, 0, 50)
